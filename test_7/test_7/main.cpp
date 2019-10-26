c8b7764282dbaf833fa3a20b19e443d5bbbfa149
lantian-fz <2459396922@qq.com>
2019-10-26 17:59:30 +0800

    练习代码

---
+++ test_7/test_7/main.cpp
@@ -0,0 +1,150 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+
+/////////////////////////////////////////
+//length()
+//operator[]
+//operator+
+//operator+=
+//operator > < >= <= == !=
+/////////////////////////////////////////
+class String
+{
+public:
+	String(char *str = "")
+	{
+		m_data = new char[strlen(str) + 1];
+		strcpy(m_data, str);
+	}
+	String(const String &s)
+	{
+		m_data = new char[strlen(s.m_data) + 1];
+		strcpy(m_data, s.m_data);
+	}
+	String& operator=(const String &s)
+	{
+		if (this != &s)
+		{
+			free(m_data);
+			m_data = new char[strlen(s.m_data) + 1];
+			strcpy(m_data, s.m_data);
+		}
+		return *this;
+	}
+
+	char& operator[](int i)
+	{
+		return m_data[i];
+	}
+
+	String& operator+(const String &s)
+	{
+		char *tmp = new char[strlen(m_data)];
+		strcpy(tmp, m_data);
+		m_data = new char[strlen(m_data) + strlen(s.m_data) + 1];
+		strcpy(m_data, tmp);
+		strcat(m_data, s.m_data);
+		return *this;
+	}
+
+	String& operator+=(const String &s)
+	{
+		char *tmp = new char[strlen(m_data)];
+		strcpy(tmp, m_data);
+		m_data = new char[strlen(m_data) + strlen(s.m_data) + 1];
+		strcpy(m_data, tmp);
+		strcat(m_data, s.m_data);
+		return *this;
+	}
+
+	bool operator==(String &s)
+	{
+		if (lenght() != s.lenght())
+			return false;
+		for (int i = 0; i < lenght(); i++)
+		{
+			if (m_data[i] != s.m_data[i])
+				return false;
+		}
+		return true;
+	}
+
+	bool operator!=(String &s)
+	{
+		return !(*this == s);
+	}
+
+	bool operator>(const String &s)
+	{
+		if (strcmp(m_data, s.m_data) > 0)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator<(const String &s)
+	{
+		if (strcmp(m_data, s.m_data) < 0)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator>=(String &s)
+	{
+		return (*this>s) || (*this == s);
+	}
+
+	bool operator<=(String &s)
+	{
+		return (*this < s) || (*this == s);
+	}
+
+	~String()
+	{
+		delete[]m_data;
+		m_data = NULL;
+	}
+public:
+	int lenght()
+	{
+		return strlen(m_data);
+	}
+private:
+	char *m_data;
+};
+
+int main()
+{
+	String s1("Hello "); //s1[0] ==> H
+	String s2("Bit.");
+
+	cout << s1.lenght() << endl;
+
+	for (int i = 0; i<s1.lenght(); ++i)
+		cout << s1[i];
+	cout << endl;
+
+	String s = s1 + s2; //s = Hello Bit.
+
+	s1 += s2;    
+	for (int i = 0; i < s1.lenght(); i++)
+		cout << s1[i];
+	cout << endl;
+	if (s1 == s2)
+		cout << "s1 == s2" << endl;
+	if (s1 != s2)
+		cout << "s1 != s2" << endl;
+	if (s1 > s2)
+		cout << "s1 > s2" << endl;
+	if (s1 < s2)
+		cout << "s1 < s2" << endl;
+	if (s1 >= s2)
+		cout << "s1 >= s2" << endl;
+	if (s1 <= s2)
+		cout << "s1 <= s2" << endl;
+
+	return 0;
+}
