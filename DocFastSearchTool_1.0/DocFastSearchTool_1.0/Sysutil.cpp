f7f8de57179ee5ea8164855efdf036d9837952ac
lantian-fz <2459396922@qq.com>
2020-02-15 23:07:51 +0800

    练习代码

---
+++ DocFastSearchTool_1.0/DocFastSearchTool_1.0/Sysutil.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "Sysutil.h"
+
+void DirectoryList(const string& path, vector<string>& subfile, vector<string>& subdir)
+{
+	string _path = path;
+	_path += "\\*.*";
+
+}
---
+++ DocFastSearchTool_1.0/DocFastSearchTool_1.0/main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "Common.h"
+
+void Test_DirectoryList()
+{
+	string path = "E:\\比特\\课件\\C语言";//因为\为转义字符
+	vector<string> subfile;//文件名
+	vector<string> subdir;//目录名
+	
+
+}
+
+void main()
+{
+	Test_DirectoryList();
+}
