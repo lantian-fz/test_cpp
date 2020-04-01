36b5c50838eeecabf1980000a99850ecec7bf0a3
lantian-fz <2459396922@qq.com>
2020-04-01 23:07:48 +0800

    牛客-不用加减乘除做加法

---
+++ test_103/test_103/main.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//不用加减乘除做加法
+//写一个函数，求两个整数之和，要求在函数体内不得使用 + 、 - 、*、 / 四则运算符号。
+#include <iostream>
+using namespace std;
+
+int Add(int num1, int num2)
+{
+	while (num2!=0)
+	{
+		int x = num1^num2;
+		num2 = (num1&num2) << 1;
+		num1 = x;
+	}
+	return num1;
+}
+
+int main()
+{
+	int m = 0;
+	int n = 0;
+	while (cin >> m >> n)
+	{
+		cout << Add(m, n) << endl;
+	}
+	return 0;
+}
