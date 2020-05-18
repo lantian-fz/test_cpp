dd81c1fd57027d62e79abe529b15aaf846d70649
lantian-fz <2459396922@qq.com>
2020-05-18 23:42:00 +0800

    力扣-876.链表的中间结点

---
+++ test_177/test_177/main.cpp
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//力扣-876.链表的中间结点
+//给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+//如果有两个中间结点，则返回第二个中间结点。
+//示例 1：
+//输入：[1, 2, 3, 4, 5]
+//输出：此列表中的结点 3 (序列化形式：[3, 4, 5])
+//返回的结点值为 3 。(测评系统对该结点序列化表述是[3, 4, 5])。
+//注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+//ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+//
+//示例 2：
+//输入：[1, 2, 3, 4, 5, 6]
+//输出：此列表中的结点 4 (序列化形式：[4, 5, 6])
+//由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+void Insert(ListNode *&head, const int x)
+{
+	if (head == NULL)
+		head = new ListNode(x);
+	else
+		Insert(head->next, x);
+}
+
+void Clear(ListNode *&head)
+{
+	while (head != NULL)
+	{
+		ListNode* r = head;
+		head = r->next;
+		delete r;
+		r = NULL;
+	}
+}
+
+ListNode* middleNode(ListNode* head) //快慢指针法
+{
+	ListNode* p = head;
+	ListNode* q = head;
+	while (q != NULL&&q->next!=NULL)
+	{
+		p = p->next;
+		q = q->next->next;
+	}
+	return p;
+}
+int main()
+{
+	ListNode *head = NULL;
+	Insert(head, 1);
+	Insert(head, 2);
+	Insert(head, 3);
+	Insert(head, 4);
+	Insert(head, 5);
+	Insert(head, 6);
+
+	ListNode* q = middleNode(head);
+
+	while (q != NULL)
+	{
+		cout << q->val << "->";
+		q = q->next;
+	}
+	cout << "NULL" << endl;
+	Clear(head);
+
+	return 0;
+}
