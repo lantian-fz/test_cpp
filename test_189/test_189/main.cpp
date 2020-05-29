701497366f5620bb0bf68dfa58e4cf5d72f09289
lantian-fz <2459396922@qq.com>
2020-05-29 22:53:23 +0800

    力扣-10.正则表达式匹配

--- test_189/test_189/main.cpp
+++ test_189/test_189/main.cpp
@@ -40,3 +40,48 @@
 //s = "mississippi"
 //p = "mis*is*p*."
 //输出: false
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+bool isMatch(string s, string p) 
+{
+	if (p.empty()) 
+		return s.empty();
+	int m = s.size(), n = p.size();
+	vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+	dp[0][0] = true;
+	//先将p的可去开头找出来
+	for (int j = 0; j < n; j++)
+	{
+		if (p[j] == '*')
+			dp[0][j + 1] = dp[0][j - 1];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (s[i] == p[j] || p[j] == '.')
+				dp[i + 1][j + 1] = dp[i][j];
+			else if (p[j] == '*')
+			{
+				//可忽略或p可多匹配
+				dp[i + 1][j + 1] = dp[i + 1][j - 1] || (dp[i][j + 1] && (p[j - 1] == s[i] || p[j - 1] == '.'));
+			}
+			else
+				dp[i + 1][j + 1] = false;
+		}
+	}
+	return dp[m][n];
+}
+
+int main()
+{
+	string s, p;
+	while (cin >> s >> p)
+		cout << isMatch(s, p) << endl;
+
+	return 0;
+}
