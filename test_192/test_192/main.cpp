594e63ffc8a6f1807766f32b9652f04cf49f20b0
lantian-fz <2459396922@qq.com>
2020-06-05 23:55:29 +0800

    力扣-56.合并区间

---
+++ test_192/test_192/main.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//力扣-56.合并区间
+//给出一个区间的集合，请合并所有重叠的区间。
+//示例 1:
+//输入: [[1,3],[2,6],[8,10],[15,18]]
+//输出: [[1,6],[8,10],[15,18]]
+//解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+//
+//示例 2:
+//输入: [[1,4],[4,5]]
+//输出: [[1,5]]
+//解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) 
+{
+
+}
+
+int main()
+{
+
+
+	return 0;
+}
