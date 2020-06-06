d9dd63fb2c9d6501d57944a26e744e5ae2abc851
lantian-fz <2459396922@qq.com>
2020-06-06 23:36:58 +0800

    力扣-56.合并区间

--- test_192/test_192/main.cpp
+++ test_192/test_192/main.cpp
@@ -13,16 +13,44 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> merge(vector<vector<int>>& intervals) 
+//vector<vector<int>> merge_1(vector<vector<int>>& intervals) 
+//{
+//
+//}
+//ÅÅÐò+Ë«Ö¸Õë
+vector<vector<int>> merge_2(vector<vector<int>>& intervals) 
 {
-
+	sort(intervals.begin(), intervals.end());
+	vector<vector<int>> ans;
+	for (int i = 0; i < intervals.size();) 
+	{
+		int t = intervals[i][1];
+		int j = i + 1;
+		while (j < intervals.size() && intervals[j][0] <= t) 
+		{
+			t = max(t, intervals[j][1]);
+			j++;
+		}
+		ans.push_back({ intervals[i][0], t });
+		i = j;
+	}
+	return ans;
 }
-
 int main()
 {
+	vector<vector<int>> arr = { { 1, 3 }, { 2, 6 }, { 8, 10 }, { 15, 18 } };
 
+	vector<vector<int>> p = merge_2(arr);
+
+	for (size_t i = 0; i < p.size(); i++)
+	{
+		for (size_t j = 0; j < p[0].size(); j++)
+			cout << p[i][j] << " ";
+		cout << ",";
+	}
 
 	return 0;
 }
