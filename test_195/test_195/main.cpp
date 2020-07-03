bf32980f39249c1a5ca874be040700e226eb42aa
lantian-fz <2459396922@qq.com>
2020-07-03 23:04:57 +0800

    练习代码

--- test_195/test_195/main.cpp
+++ test_195/test_195/main.cpp
@@ -2,38 +2,69 @@
 #include <iostream>
 using namespace std;
 
-namespace A
+int& Add(int a, int b)
 {
-	int a;
-	void show(){cout << "aaaaaa" << endl;}
+	int c = a + b;
+	return c;
 }
-
-namespace B
-{
-	int b;
-	void show(){cout << "bbbbbb" << endl;}
-	namespace C
-	{
-		int c;
-		void show(){cout << "ccccccc" << endl;}
-	}
-}
-
-namespace A
-{
-	void showA(){cout << "AAAAAAA" << endl;}
-}
-
-using A::a;
-using namespace B;
-
 int main()
 {
-	a = 3;
-	b = 1;
-	show();
-	
-	A::showA();
-
+	int& ret = Add(1, 2);
+	Add(3, 4);
+	cout << "Add(1, 2) is :" << ret << endl;
 	return 0;
 }
+
+//void show(int a, int b = 3)
+//{
+//	cout << a << " " << b << endl;
+//}
+//
+//void show(int a, int b)
+//{
+//	cout << a << "*" << b << endl;
+//}
+//
+//int main()
+//{
+//	show(3, 4);
+//
+//	return 0;
+//}
+
+
+//namespace A
+//{
+//	int a;
+//	void show(){cout << "aaaaaa" << endl;}
+//}
+//
+//namespace B
+//{
+//	int b;
+//	void show(){cout << "bbbbbb" << endl;}
+//	namespace C
+//	{
+//		int c;
+//		void show(){cout << "ccccccc" << endl;}
+//	}
+//}
+//
+//namespace A
+//{
+//	void showA(){cout << "AAAAAAA" << endl;}
+//}
+//
+//using A::a;
+//using namespace B;
+//
+//int main()
+//{
+//	a = 3;
+//	b = 1;
+//	show();
+//	
+//	A::showA();
+//
+//	return 0;
+//}
