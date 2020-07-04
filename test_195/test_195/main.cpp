4b24b74da6c288e9d122f5937e7793c36fd53ac0
lantian-fz <2459396922@qq.com>
2020-07-04 22:50:22 +0800

    练习代码

--- test_195/test_195/main.cpp
+++ test_195/test_195/main.cpp
@@ -2,18 +2,58 @@
 #include <iostream>
 using namespace std;
 
-int& Add(int a, int b)
+void f(int){ cout << "f(int)" << endl; }
+void f(int*){ cout << "f(int*)" << endl; }
+void main()
 {
-	int c = a + b;
-	return c;
+	f(0);
+	f(NULL);
+	f((int*)NULL);
 }
-int main()
-{
-	int& ret = Add(1, 2);
-	Add(3, 4);
-	cout << "Add(1, 2) is :" << ret << endl;
-	return 0;
-}
+
+//void TestFor(int array[])
+//{
+//	for (auto& e : array)
+//		cout << e << endl;
+//}
+//
+//void main()
+//{
+//	int arr[] = { 5, 2, 8, 1, 0 };
+//	for (auto x : arr)
+//		cout << x << " ";
+//}
+
+//int main()
+//{
+//	int x = 20;
+//	auto a = &x;//这里auto推导出的类型为int*
+//	auto* b = &x;//而这里推导出的为int型，*已经有了
+//	auto& c = x;//这里相当于 int& c = x;
+//
+//	cout << typeid(a).name() << endl;
+//	cout << typeid(b).name() << endl;
+//	cout << typeid(c).name() << endl;
+//
+//	*a = 3;
+//	*b = 5;
+//	c = 9;
+//
+//	return 0;
+//}
+
+//int& Add(int a, int b)
+//{
+//	int c = a + b;
+//	return c;
+//}
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	Add(3, 4);
+//	cout << "Add(1, 2) is :" << ret << endl;
+//	return 0;
+//}
 
 //void show(int a, int b = 3)
 //{
