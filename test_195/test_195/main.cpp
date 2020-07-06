6a2be0db0ddd8108a4dacd29537279b413953d75
lantian-fz <2459396922@qq.com>
2020-07-06 21:32:01 +0800

    牛客-二维数组中的查找

--- test_195/test_195/main.cpp
+++ test_195/test_195/main.cpp
@@ -1,15 +1,67 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+//牛客-二维数组中的查找
+//在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+//请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+//
+//1 2 8  9
+//2 4 9  12
+//4 7 10 13
+//6 8 11 15
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void f(int){ cout << "f(int)" << endl; }
-void f(int*){ cout << "f(int*)" << endl; }
+bool Find(int target, vector<vector<int>> array) 
+{
+	bool flag = false;
+	int rows = array.size();
+	int cols = array[0].size();
+	if (rows > 0 && cols > 0)
+	{
+		int row = 0, col = cols - 1;
+		while (row < rows&&col >= 0)
+		{
+			if (array[row][col] == target)
+			{
+				flag = true;
+				break;
+			}
+			else if (array[row][col]>target)
+				--col;
+			else
+				++row;
+		}
+	}
+	return flag;
+}
+
 void main()
 {
-	f(0);
-	f(NULL);
-	f((int*)NULL);
+	vector<vector<int>> arr = { { 1, 2, 8, 9 },
+								{ 2, 4, 9, 12 },
+								{ 4, 7, 10, 13 },
+								{ 6, 8, 11, 15 } };
+	cout << Find(7, arr) << endl;
 }
+
+//int main(void)
+//{
+//	const int a = 10;
+//	int * p = (int *)(&a);
+//	*p = 20;
+//	cout << "a = " << a << ", *p = " << *p << endl;
+//	return 0;
+//}
+
+//void f(int){ cout << "f(int)" << endl; }
+//void f(int*){ cout << "f(int*)" << endl; }
+//void main()
+//{
+//	f(0);
+//	f(NULL);
+//	f((int*)NULL);
+//}
 
 //void TestFor(int array[])
 //{
