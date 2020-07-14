0fc7d0f0b8d2cba91c5e24ea833c823301b6511b
lantian-fz <2459396922@qq.com>
2020-07-14 14:11:19 +0800

    牛客-旋转数组的最小数字-剑指offer-8

--- test_202/test_202/main.cpp
+++ test_202/test_202/main.cpp
@@ -11,25 +11,71 @@
 #include <vector>
 using namespace std;
 
-int minNumberInRotateArray(vector<int> rotateArray) 
+//二分法，并没有解决有重复数字的问题
+int minNumberInRotateArray_1(vector<int> rotateArray) 
 {
 	if (rotateArray.size() == 0)
 		return 0;
 	int start = 0;
 	int end = rotateArray.size() - 1;
-	while ((start + 1) != end)
+	int mid = 0;
+	while (rotateArray[start]>=rotateArray[end])
 	{
-		int mid = (start + end) / 2;
+		if (start + 1 == end)
+		{
+			mid = end;
+			break;
+		}
+		mid = start + (end - start) / 2;
 		if (rotateArray[mid] > rotateArray[start])
 			start = mid;
 		else if (rotateArray[mid] < rotateArray[end])
 			end = mid;
 	}
-	return rotateArray[end];
+	return rotateArray[mid];
+}
+
+int minInOrder(vector<int>& arr, int index1, int index2)
+{
+	int n = arr[index1];
+	for (int i = index1 + 1; i <= index2; ++i)
+	{
+		if (n > arr[i])
+			n = arr[i];
+	}
+	return n;
+}
+
+int minNumberInRotateArray_2(vector<int> &rotateArray)
+{
+	if (rotateArray.size() == 0)
+		return 0;
+	int index1 = 0;
+	int index2 = rotateArray.size() - 1;
+	int mid = index1;
+	while (rotateArray[index1] >= rotateArray[index2])
+	{
+		if (index2 - index1 == 1)
+		{
+			mid = index2;
+			break;
+		}
+		mid = (index1 + index2) / 2;
+
+		//如果三个数相等，则在该范围内顺序查找
+		if (rotateArray[index1] == rotateArray[index2] && rotateArray[mid] == rotateArray[index1])
+			return minInOrder(rotateArray, index1, index2);
+		if (rotateArray[mid] >= rotateArray[index1])
+			index1 = mid;
+		else if (rotateArray[mid] <= rotateArray[index2])
+			index2 = mid;
+	}
+	return rotateArray[mid];
 }
 
 void main()
 {
-	vector<int> arr = { 3, 4, 5, 1, 2 };
-	cout << minNumberInRotateArray(arr) << endl;
+	//数组{0,1,1,1,1}的两个旋转{1,1,1,0,1}和{1,0,1,1,1}
+	vector<int> arr = { 3, 4, 5, 2, 2 };
+	cout << minNumberInRotateArray_2(arr) << endl;
 }
