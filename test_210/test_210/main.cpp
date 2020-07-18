0277b500278e3a6fcccb8541d16271040129dc00
lantian-fz <2459396922@qq.com>
2020-07-18 23:18:18 +0800

    剑指offer-包含min函数的栈

---
+++ test_210/test_210/main.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//剑指offer-包含min函数的栈
+//定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+
+class Solution {
+public:
+	void push(int value) {
+
+	}
+	void pop() {
+
+	}
+	int top() {
+
+	}
+	int min() {
+
+	}
+};
+
+void main()
+{
+
+}
