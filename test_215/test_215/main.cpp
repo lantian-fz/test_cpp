23601cebb3dfc0287cbafbe36c522531eeb8029b
lantian-fz <2459396922@qq.com>
2020-07-25 23:57:29 +0800

    剑指offer-二叉树中和为某一值的路径

--- test_215/test_215/main.cpp
+++ test_215/test_215/main.cpp
@@ -15,11 +15,45 @@ struct TreeNode
 	TreeNode(int x) :val(x), left(NULL), right(NULL) {}
 };
 
+void FindPath(TreeNode* root, int expectNumber, vector<vector<int>>& ret, vector<int>&arr, int& currentSun)
+{
+	currentSun += root->val;
+	arr.push_back(root->val);
+	if (currentSun == expectNumber&&root->left == nullptr&&root->right == nullptr)
+		ret.push_back(arr);
+
+	if (root->left != nullptr)
+		FindPath(root->left, expectNumber, ret, arr, currentSun);
+	if (root->right != nullptr)
+		FindPath(root->right, expectNumber, ret, arr, currentSun);
+
+	currentSun -= root->val;
+	arr.pop_back();
+}
+
 vector<vector<int>> FindPath(TreeNode* root, int expectNumber) 
 {
-
+	vector<vector<int>> ret;
+	vector<int> arr;
+	int currentSun = 0;
+	FindPath(root, expectNumber, ret, arr, currentSun);
+	return ret;
 }
+
 void main()
 {
+	TreeNode* root = new TreeNode(10);
+	root->left = new TreeNode(5);
+	root->right = new TreeNode(12);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(7);
 
+	vector<vector<int>>& ret = FindPath(root, 22);
+
+	for (auto i : ret)
+	{
+		for (auto j : i)
+			cout << j << " ";
+		cout << endl;
+	}
 }
